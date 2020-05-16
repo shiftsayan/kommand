86b29fc27e910d9bfa462a5f87650342f8505801
Sayan Chaudhry <sayan.chaudhry@gmail.com>
2020-05-16 03:54:25 -0400

    Add 4 websites

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action) {
-      switch (message.action) {
-        case 'kommand':
-            runKommand();
+        switch (message.action) {
+            case 'kommand':
+                runKommand();
         }
     }
 });
 
+function getElementByAriaLabel(label) {
+    return document.querySelectorAll("[aria-label^='" + label + "']")[0];
+}
+
+function getElementByType(label) {
+    return document.querySelectorAll("[type^='" + label + "']")[0];
+}
+
 function runKommand() {
-    
+    switch (window.location.hostname) {
+        case 'www.google.com':
+            var bar = getElementByAriaLabel('Search');
+            break;
+        case 'www.facebook.com':
+            var bar = getElementByAriaLabel('Search Facebook');
+            break;
+        case 'github.com':
+            var bar = getElementByAriaLabel('Search or jump to…');
+            break;
+        case 'www.gradescope.com':
+            var bar = getElementByType('search')
+            break;
+    }
+    bar.focus();
+    bar.select();
 }
